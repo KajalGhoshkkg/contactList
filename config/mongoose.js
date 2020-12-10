4e9823c8d203f3592d51f82de812699331b97fd3
KajalGhoshkkg <kajolghoshkkg@gmail.com>
2020-12-10 10:47:07 +0530

    4th commit on online db

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/contact_list");
+// mongoose.connect("mongodb://localhost/contact_list");
+
+mongoose.connect(
+  "mongodb+srv://kkg:kkg@cluster0.cz2sr.mongodb.net/contact_list?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 const db = mongoose.connection;
 
